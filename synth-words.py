c64cad92c6aebddbf64ac39b528c0c57bbb0660a
Kathryn Schuler <kathryn.schuler@gmail.com>
2016-05-07 20:05:36 -0400

    add speech synthesizer

---
+++ synth-words.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+from os import system, mkdir
+
+path = 'synth-sounds/'
+voice = 'Alex'
+rate = 250
+wordList = [
+    "hello",
+    "goodbye",
+    "yesterday",
+    "one fine day"]
+
+mkdir(path)
+for word in wordList :
+    system("say -v"+voice+" -r "+rate+" "+word+" -o "+path+word+".wav")
